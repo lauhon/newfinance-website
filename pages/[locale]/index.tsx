3117153a1f1cff44784d1843e5b97af22f87ef4f
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-04-04 22:10:53 +0200

    initial configuration + header

--- pages/[locale]/index.tsx
+++ pages/[locale]/index.tsx
@@ -1,6 +1,3 @@
-import FAQ from "~/components/home/faq";
-import Qualities from "~/components/home/qualities";
-import Splash from "~/components/home/splash";
 import Header from "~/components/layout/header";
 import Layout from "~/components/layout/layout";
 import { getStaticPaths, makeStaticProps } from "~/lib/ui/language/getStatic";
@@ -12,13 +9,7 @@ const Home = () => {
     <Layout>
       <Header />
 
-      <main className="space-y-24 bg-white pb-8">
-        <Splash />
-        <Qualities />
-        <div>
-          <FAQ />
-        </div>
-      </main>
+      <main className="space-y-24 bg-white pb-8"></main>
     </Layout>
   );
 };
