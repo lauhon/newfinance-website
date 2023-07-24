241cbc4ed2337946631757bc137efb1c9db43640
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-07-24 11:31:13 +0200

    changed hover in videoarea

--- components/home/video.tsx
+++ components/home/video.tsx
@@ -1,16 +1,19 @@
 import { useTranslation } from "next-i18next";
+import Link from "next/link";
 import ButtonArrow from "../shared/buttons/button-arrow";
 
 const VideoArea = () => {
   const { t } = useTranslation("common");
 
   return (
-    <section className="flex items-center px-4 md:px-14 lg:px-28 md:mt-32 md:mb-32 mt-16 mb-16 flex-col">
-      <div className="flex justify-center items-center bg-[#EDEEF0] rounded-2xl w-[1000px] max-w-full min-h-[500px]">
-        <div>
-          <ButtonArrow href="login" text={t("header.startButton")} />
+    <section className="flex items-center px-4 md:px-14 lg:px-28 md:mt-32 md:mb-32 mt-16 mb-16 flex-col overflow-hidden">
+      <Link href="login" className="cursor-default">
+        <div className="flex justify-center items-center bg-[#EDEEF0] rounded-2xl w-[1000px] max-w-full min-h-[500px] group">
+          <div className="md:group-hover:scale-105 transition duration-300 object-cover">
+            <ButtonArrow href="login" text={t("header.startButton")} />
+          </div>
         </div>
-      </div>
+      </Link>
     </section>
   );
 };
