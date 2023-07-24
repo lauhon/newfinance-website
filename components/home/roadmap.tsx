be2a27323597f39dcc9b9816b899ca843113cd49
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-07-24 15:56:29 +0200

    added roadmap

--- components/home/roadmap.tsx
+++ components/home/roadmap.tsx
@@ -1,4 +1,5 @@
 import { Trans, useTranslation } from "next-i18next";
+import * as Icon from "react-feather";
 import BabyText from "../shared/text/baby-text";
 
 const RoadmapArea = () => {
@@ -16,8 +17,91 @@ const RoadmapArea = () => {
           </span>
         </h2>
       </div>
-      <div className="flex justify-center items-center w-[600px] max-w-full h-[500px] bg-gradient-to-br from-[#909090] to-5% rounded-lg p-[0.5px]">
-        <div className="w-full h-full bg-gradient-to-br from-[#343636] via-[#3A3A3A] to-[#3A3A3A] rounded-lg"></div>
+      <div className="flex justify-center items-center w-[600px] max-w-full bg-gradient-to-br from-[#909090] to-5% rounded-lg p-[0.5px]">
+        <div className="w-full h-full bg-gradient-to-br from-[#343636] via-[#3A3A3A] to-[#3A3A3A] rounded-lg p-2 ">
+          <div className="flex flex-row items-center bg-white w-full rounded-xl p-4 border-1 border-[#2C2C2C] drop-shadow transition hover:opacity-90">
+            <div className="flex justify-center items-center rounded-lg bg-[#F8F8F8] p-3 mr-4">
+              <Icon.Star size={22} color="#0D0E12" />
+            </div>
+            <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular">
+              Bank-like usability
+            </span>
+          </div>
+
+          <div className="flex flex-row items-center bg-white w-full rounded-xl p-4 border-1 border-[#2C2C2C] drop-shadow mt-1.5 transition hover:opacity-90">
+            <div className="flex justify-center items-center rounded-lg bg-[#F8F8F8] p-3 mr-4">
+              <Icon.Lock size={22} color="#0D0E12" />
+            </div>
+            <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular">
+              MPC secured Self-custodian
+            </span>
+          </div>
+
+          <div className="flex flex-row items-center bg-white w-full rounded-xl p-4 border-1 border-[#2C2C2C] drop-shadow mt-1.5 transition hover:opacity-90">
+            <div className="flex justify-center items-center rounded-lg bg-[#F8F8F8] p-3 mr-4">
+              <Icon.Send size={22} color="#0D0E12" />
+            </div>
+            <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular">
+              Send and receive borderless
+            </span>
+          </div>
+
+          <div className="flex flex-row items-center justify-between bg-white w-full rounded-xl p-4 border-1 border-[#2C2C2C] drop-shadow mt-1.5 transition hover:opacity-90 group">
+            <div className="flex flex-row items-center group-hover:translate-x-[-1px] transition">
+              <div className="flex justify-center items-center rounded-lg bg-[#F8F8F8] p-3 mr-4">
+                <Icon.DollarSign size={22} color="#0D0E12" />
+              </div>
+              <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular">
+                Buy and Sell Bitcoin directly
+              </span>
+            </div>
+            <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular text-[#348FE2] group-hover:translate-x-px transition">
+              Q2 23’{" "}
+            </span>
+          </div>
+
+          <div className="flex flex-row items-center justify-between bg-white w-full rounded-xl p-4 border-1 border-[#2C2C2C] drop-shadow mt-1.5 transition hover:opacity-90 group">
+            <div className="flex flex-row items-center group-hover:translate-x-[-1px] transition">
+              <div className="flex justify-center items-center rounded-lg bg-[#F8F8F8] p-3 mr-4">
+                <Icon.Zap size={22} color="#0D0E12" />
+              </div>
+              <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular">
+                Lightning network
+              </span>
+            </div>
+            <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular text-[#348FE2] group-hover:translate-x-px transition">
+              Q3 23’{" "}
+            </span>
+          </div>
+
+          <div className="flex flex-row items-center justify-between bg-white w-full rounded-xl p-4 border-1 border-[#2C2C2C] drop-shadow mt-1.5 transition hover:opacity-90 group">
+            <div className="flex flex-row items-center group-hover:translate-x-[-1px] transition">
+              <div className="flex justify-center items-center rounded-lg bg-[#F8F8F8] p-3 mr-4">
+                <Icon.Repeat size={22} color="#0D0E12" />
+              </div>
+              <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular">
+                Recurring savings
+              </span>
+            </div>
+            <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular text-[#348FE2] group-hover:translate-x-px transition">
+              Q3 23’{" "}
+            </span>
+          </div>
+
+          <div className="flex flex-row items-center justify-between bg-white w-full rounded-xl p-4 border-1 border-[#2C2C2C] drop-shadow mt-1.5 transition hover:opacity-90 group">
+            <div className="flex flex-row items-center group-hover:translate-x-[-1px] transition">
+              <div className="flex justify-center items-center rounded-lg bg-[#F8F8F8] p-3 mr-4">
+                <Icon.CreditCard size={22} color="#0D0E12" />
+              </div>
+              <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular">
+                Iban & Sepa compatible
+              </span>
+            </div>
+            <span className="text-[0.8rem] sm:text-[1.1rem] text-left font-manrope font-regular text-[#348FE2] group-hover:translate-x-px transition">
+              Q4 23’{" "}
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   );
