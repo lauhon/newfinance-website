37bc997a151efb92455dffc1c2fe666d29efb042
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-07-25 11:34:21 +0200

    fixed video area on smaller devices

--- components/home/video.tsx
+++ components/home/video.tsx
@@ -7,7 +7,10 @@ const VideoArea = () => {
 
   return (
     <section className="flex items-center px-4 md:px-14 lg:px-28 md:mt-32 md:mb-32 mt-16 mb-16 flex-col overflow-hidden">
-      <Link href="login" className="cursor-default">
+      <Link
+        href="login"
+        className="cursor-default w-[1000px] max-w-full min-h-[500px]"
+      >
         <div className="flex justify-center items-center bg-[#EDEEF0] rounded-2xl w-[1000px] max-w-full min-h-[500px] group">
           <div className="md:group-hover:scale-105 transition duration-300 object-cover">
             <ButtonArrow href="login" text={t("header.startButton")} />
