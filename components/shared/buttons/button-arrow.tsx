8c5a5812205d195ced37114bf1c87f32ddb02d36
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-08-01 22:09:56 +0200

    fixed button color

--- components/shared/buttons/button-arrow.tsx
+++ components/shared/buttons/button-arrow.tsx
@@ -11,7 +11,7 @@ const ButtonArrow = ({ text = "Text", onClick, href }: ButtonProps) => {
   return (
     <Link
       href={`/${href}`}
-      className="flex align-middle bg-gradient-to-r from-[#3A3C3A] to-[#040504] pl-4 md:pl-5 hover:opacity-80 font-regular rounded-full items-center transition-all duration-200 cursor-default text-white text-sm font-manrope font-regular tracking-tight"
+      className="flex align-middle bg-gradient-to-r from-[#212221] to-[#040504] pl-4 md:pl-5 hover:opacity-80 font-regular rounded-full items-center transition-all duration-200 cursor-default text-white text-sm font-manrope font-regular tracking-tight"
     >
       {text}
       <div className="bg-white rounded-full ml-2.5 w-8 h-8 my-1 mr-1 flex items-center justify-center">
