ecbf9e187195f6331c6e0ef7adc0abb106a90029
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-10-18 11:02:58 +0200

    added alpha banner

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import { Trans, useTranslation } from "next-i18next";
 import Head from "next/head";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import ButtonArrow from "../shared/buttons/button-arrow";
 
 const description = "NewFinance. Invest and secure your future with Bitcoin.";
@@ -14,6 +14,8 @@ const Layout = ({ children }: Props) => {
   }
   // <body class="h-full overflow-x-hidden text-base bg-default text-default selection:bg-secondary selection:text-white">
   const { i18n } = useTranslation("home");
+  const [showBannerAlpha, setShowBannerAlpha] = useState(true);
+
   return (
     <>
       <Head>
@@ -47,27 +49,33 @@ const Layout = ({ children }: Props) => {
         <link rel="alternate" href="http://example.com" hrefLang="en" />
         <link rel="alternate" href="http://example.com/de" hrefLang="de" />
       </Head>
-      <div className="fixed flex space-x-8 align-middle flex-row left-1/2 -translate-x-1/2 z-50 bg-white px-6 py-4 rounded-3xl bottom-8 shadow-2xl">
-        <div>
-          <div className="font-manrope text-sm font-semibold">
+      {showBannerAlpha && (
+        <div className="fixed flex space-y-4 lg:space-y-0 lg:space-x-8 align-middle flex-col lg:flex-row lg:left-1/2 lg:-translate-x-1/2 z-50 bg-white mx-8 lg:mx-0 px-6 py-8 lg:py-4 rounded-3xl bottom-8 shadow-2xl">
+          <div className="flex lg:w-[400px] font-manrope text-sm font-semibold">
             <Trans i18nKey="layout.banner" />
           </div>
+          <div className="flex space-y-4 md:space-y-0 md:space-x-8 flex-col md:flex-row">
+            <div className="flex">
+              <ButtonArrow
+                color="#EEF1F5"
+                text={i18n.language == "en" ? "Ok" : "Ok"}
+                inverted={true}
+                icon="Check"
+                onClick={() => setShowBannerAlpha(false)}
+              />
+            </div>
+            <div className="flex">
+              <ButtonArrow
+                text={
+                  i18n.language == "en" ? "Notify me" : "Benachrichtige mich"
+                }
+                icon="ArrowRight"
+                href="login"
+              />
+            </div>
+          </div>
         </div>
-        <div>
-          <ButtonArrow
-            color="#EEF1F5"
-            text={i18n.language == "en" ? "Ok" : "Ok"}
-            inverted={true}
-            icon="Check"
-          />
-        </div>
-        <div>
-          <ButtonArrow
-            text={i18n.language == "en" ? "Notify me" : "Benachrichtige mich"}
-            icon="ArrowRight"
-          />
-        </div>
-      </div>
+      )}
       {children}
     </>
   );
