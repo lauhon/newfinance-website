d9e17377122b1a8064140b2f0014b9f85d2f8dcb
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-10-18 09:59:41 +0200

    mailjet add contact + add to contactlist

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,6 +1,7 @@
 const config = {
-  mailJetUser: process.env.MAILJET_USER,
-  mailJetPassword: process.env.MAILJET_PASSWORD,
+  mailJetContactList: process.env.MAILJET_CONTACT_LIST || "",
+  mailJetUser: process.env.MAILJET_USER || "",
+  mailJetPassword: process.env.MAILJET_PASSWORD || "",
 };
 
 export default config;
--- services/mailjet/email-service.ts
+++ services/mailjet/email-service.ts
@@ -1,29 +1,48 @@
-import { createAuthBasic } from "~/lib/api/auth";
-import { AddContactResponse } from "~/lib/api/types/mailjet/contact";
-import { MailjetError } from "./mailjet";
+import config from "~/lib/api/config";
+import { MailjetContact, MailjetError } from "./mailjet";
 
 export const addContact = async (
   email: string
 ): Promise<AddContactResponse> => {
-  const response = await fetch(mailApi + contactEndpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `${createAuthBasic()}`,
-    },
-    body: JSON.stringify({
-      email,
-    }),
+  const response = await fetchFromMailjet(contactEndpoint, {
+    email,
   });
 
   if (!response.ok) {
-    return evaluateMailjetError(await response.json());
+    return evaluateMailjetError((await response.json()) as MailjetError);
   }
+
+  const result = await response.json();
+
+  const [newContact] = result.Data as MailjetContact[];
+
+  console.debug({ newContact }, "Successfully created Mailject contact");
+
+  const listResponse = await addUserToList(newContact);
+
+  if (!listResponse.ok) {
+    return evaluateMailjetError((await listResponse.json()) as MailjetError);
+  }
+
+  console.debug(
+    { list: await listResponse.json() },
+    "Added Mailjet contact to list"
+  );
 
   return {
     status: 200,
     message: email,
   };
+};
+
+const addUserToList = async (contact: MailjetContact) => {
+  const response = await fetchFromMailjet(contactListEndpoint, {
+    ContactID: contact.ID,
+    ContactAlt: contact.Email,
+    ListID: config.mailJetContactList,
+  });
+
+  return response;
 };
 
 const evaluateMailjetError = (error: MailjetError): AddContactResponse => {
@@ -40,5 +59,33 @@ const evaluateMailjetError = (error: MailjetError): AddContactResponse => {
   };
 };
 
+const createAuthBasic = () => {
+  return (
+    "Basic " +
+    Buffer.from(config.mailJetUser + ":" + config.mailJetPassword).toString(
+      "base64"
+    )
+  );
+};
+
 const mailApi = "https://api.mailjet.com/v3/REST";
 const contactEndpoint = "/contact ";
+const contactListEndpoint = "/listrecipient ";
+
+export type AddContactResponse = {
+  status: AddContactStatus;
+  message: string;
+};
+
+export type AddContactStatus = 200 | 400 | 409 | 500;
+
+const fetchFromMailjet = (endpoint: string, body: Object) => {
+  return fetch(mailApi + endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `${createAuthBasic()}`,
+    },
+    body: JSON.stringify(body),
+  });
+};
