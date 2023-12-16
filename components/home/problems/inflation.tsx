ff68e91502c21a457566314191892074f955e95e
Mathias Nahringbauer <mathias.nahringbauer@gmail.com>
2023-12-16 16:12:46 +0100

    fix(website): fixed sizes of inflation area with graph

--- components/home/problems/inflation.tsx
+++ components/home/problems/inflation.tsx
@@ -1,4 +1,3 @@
-import { Check } from "lucide-react";
 import { Trans, useTranslation } from "next-i18next";
 import Image from "next/image";
 import bg from "../../../public/images/BackgroundInflation.png";
@@ -8,21 +7,23 @@ const Inflation = () => {
   const InflationGraph = require("~/public/images/InflationGraph.png");
 
   return (
-    <section className="flex flex-col items-center px-4 md:px-14 lg:px-28 md:pt-32 md:pb-32 pt-16 pb-16 space-y-8 md:space-y-16">
-      <div className="text-left  w-[1000px] max-w-full">
-        <h2 className="text-[2rem] sm:text-[2.4rem] font-manrope font-semibold leading-tight mt-2">
+    <section className="flex flex-col items-center px-4 md:px-8  pb-16 space-y-8 md:space-y-16">
+      <div className="flex border-b-1 pb-24 justify-center">
+        <h3 className="flex text-2xl font-manrope font-medium text-black text-center md:w-5/6">
+          <Trans i18nKey="inflation.quote" />
+        </h3>
+      </div>
+      <div className="text-center w-full justify-center items-center md:w-2/3 pt-16">
+        <h2 className="text-[3.2rem] md:text-[4rem] lg:text-[5rem] text-default font-manrope font-semibold tracking-tight leading-none">
           <Trans i18nKey="inflation.headline_1" />
           <br />
-          <span className="text-[#757679]">
-            <Trans i18nKey="inflation.headline_2" />
-          </span>
         </h2>
-        <p className=" text-base font-manrope font-medium text-[#8E8D95] tracking-tighty mt-4">
+        <p className="px-6 text-xl font-manrope font-medium text-black text-center mt-12">
           <Trans i18nKey="inflation.headline_text" />
         </p>
       </div>
       <div
-        className="flex flex-col md:flex-row space-x-6 space-y-16 items-center py-16 px-6 sm:px-8 md:px-16 bg-[#EDEEF0] rounded-2xl w-[1000px] max-w-full min-h-[500px]"
+        className="flex flex-col space-y-12 items-center py-32 px-6 sm:px-8 md:px-16 bg-[#EDEEF0] rounded-2xl w-full min-h-[800px]"
         style={{
           backgroundImage: `url(${bg.src})`,
           backgroundSize: "cover",
@@ -31,19 +32,18 @@ const Inflation = () => {
           backgroundPositionY: "bottom",
         }}
       >
-        <div>
-          <h2 className="text-[1.9rem] sm:text-[2.3rem] font-manrope font-semibold leading-tight text-white">
-            <Trans i18nKey="inflation.subheadline" />
-          </h2>
-          <p className=" text-base font-manrope font-medium text-white mt-4">
-            <Trans i18nKey="inflation.text" />
-          </p>
-          <div className="flex flex-col mt-4 items-start md:items-start">
+        <div className="flex-1 justify-end items-end">
+          <Image
+            src={InflationGraph}
+            height={450}
+            alt="Graph about Inflation and Bitcoin"
+            className="flex"
+          />
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-col items-center">
             <div className="mt-2">
-              <span className="flex flex-row items-center text-base font-manrope font-medium text-white">
-                <div className="flex w-4 h-4 bg-white rounded-full items-center justify-center mr-2">
-                  <Check size={8} color="black" />
-                </div>
+              <span className="flex flex-row items-center text-center text-base font-manrope font-medium text-white">
                 <span>
                   <Trans i18nKey="inflation.benefit1_1" />
                   <span className="text-[#B5B5B5]">
@@ -51,14 +51,11 @@ const Inflation = () => {
                   </span>
                 </span>
               </span>
-              <div className="w-full h-[1px] bg-gradient-to-r from-[#53545B] mt-2"></div>
+              <div className="w-full h-[1px] bg-gradient-to-r from-[#cdcdcd00] via-[#cdcdcd] mt-2"></div>
             </div>
 
             <div className="mt-2">
-              <span className="flex flex-row items-center text-base font-manrope font-medium text-white">
-                <div className="flex w-4 h-4 bg-white rounded-full items-center justify-center mr-2">
-                  <Check size={8} color="black" />
-                </div>
+              <span className="flex flex-row items-center text-center text-base font-manrope font-medium text-white">
                 <span>
                   <Trans i18nKey="inflation.benefit2_1" />
                   <span className="text-[#B5B5B5]">
@@ -66,14 +63,11 @@ const Inflation = () => {
                   </span>
                 </span>
               </span>
-              <div className="w-full h-[1px] bg-gradient-to-r from-[#53545B] mt-2"></div>
+              <div className="w-full h-[1px] bg-gradient-to-r from-[#cdcdcd00] via-[#cdcdcd] mt-2"></div>
             </div>
 
             <div className="mt-2">
-              <span className="flex flex-row items-center text-base font-manrope font-medium text-white">
-                <div className="flex w-4 h-4 bg-white rounded-full items-center justify-center mr-2">
-                  <Check size={8} color="black" />
-                </div>
+              <span className="flex flex-row items-center text-center text-base font-manrope font-medium text-white">
                 <span>
                   <Trans i18nKey="inflation.benefit3_1" />
                   <span className="text-[#B5B5B5]">
@@ -82,16 +76,6 @@ const Inflation = () => {
                 </span>
               </span>
             </div>
-          </div>
-        </div>
-        <div>
-          <Image
-            src={InflationGraph}
-            alt="Graph about Inflation and Bitcoin"
-            className=""
-          />
-          <div className="mt-4 text-right text-[0.65rem] text-[#B5B5B5] font-manrope font-semibold tracking-tight">
-            <Trans i18nKey="inflation.notifier" />
           </div>
         </div>
       </div>
